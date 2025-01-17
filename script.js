119f2ff6006307c9e59dae66975339a40668ba23
Matthew Higgins <mthiggins1s@semo.edu>
2025-01-16 18:16:01 -0600

    changed `color` to `favColor`

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let age = 23;
 let name = `Matthew`;
-let color = `green`;
-console.log(`My name is ${name}, I am ${age} years old and my favorite color is ${color}.`);
+let favColor = `green`;
+console.log(`My name is ${name}, I am ${age} years old and my favorite color is ${favColor}.`);
 
 // Output: My name is Matthew, I am 23 years old and my favorite color is green.
