f8fcd912968f3a4c8492e5d2c425b8fd7679a045
Matthew Higgins <mthiggins1s@semo.edu>
2025-01-16 18:41:06 -0600

    changed the script.js file

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let favColor = `green`;
 console.log(`My name is ${name}, I am ${age} years old and my favorite color is ${favColor}.`);
 
 // Output: My name is Matthew, I am 23 years old and my favorite color is green.
+
+let lastName = `Higgins`;
+let firstName = `Matthew`;
+let fullName = `${firstName} ${lastName}`;
+console.log(`My full name is ${fullName}`);
+
+// Output: My full name is Matthew Higgins
