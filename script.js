e8d57055def09bb70df4d48db7827b830f041d59
Matthew Higgins <mthiggins1s@semo.edu>
2025-01-16 18:14:28 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let age = 23;
+let name = `Matthew`;
+let color = `green`;
+console.log(`My name is ${name}, I am ${age} years old and my favorite color is ${color}.`);
+
+// Output: My name is Matthew, I am 23 years old and my favorite color is green.
